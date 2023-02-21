1ce643fd747d8f94cc112a07b9271bdd289ba82e
Diego Alexandre <ddsa.1988@outlook.com>
2023-02-20 22:18:01 -0300

    commit

---
+++ Chapter 003 - Introduction To Node JS/main.js
@@ -0,0 +1,36 @@
+"use strict";
+
+const prompt = require("prompt-sync")();
+
+let maxNumbers = 0;
+let counter = 0;
+let numbers = [];
+
+while (true) {
+    maxNumbers = Number(prompt("How many numbers would you like to sum? "));
+
+    if (Number.isFinite(maxNumbers) && maxNumbers > 0) {
+        break;
+    }
+
+    console.log("Invalid number!");
+}
+
+console.log();
+
+while (counter < maxNumbers) {
+    numbers[counter] = Number(prompt(`Type number ${counter + 1}: `));
+
+    if (Number.isFinite(numbers[counter])) {
+        counter++;
+    } else {
+        console.log("Invalid number!");
+    }
+}
+
+const sum = numbers.reduce(
+    (previousValue, currentValue) => previousValue + currentValue
+);
+
+console.log();
+console.log(`The sum is ${sum}.`);
