3a7cbdd84f8a8ac7e9e12fe1f456048890b269f1
Diego Alexandre <ddsa.1988@outlook.com>
2023-02-27 12:53:04 -0300

    commit

--- Chapter 004 - Conditions/002 - Example Calculate Weight/main.js
+++ Chapter 004 - Conditions/002 - Example Calculate Weight/main.js
@@ -3,7 +3,7 @@
 const inName = document.querySelector("#inName");
 const inMale = document.querySelector("#inMale");
 const inFemale = document.querySelector("#inFemale");
-const inHight = document.querySelector("#inHight");
+const inHeight = document.querySelector("#inHeight");
 const inSubmit = document.querySelector("#inSubmit");
 const inReset = document.querySelector("#inReset");
 const outResponse = document.querySelector("#outResponse");
@@ -18,19 +18,19 @@ inSubmit.addEventListener("click", function (event) {
     event.preventDefault();
 
     const name = String(inName.value).trim();
-    const hight = Number(inHight.value);
+    const height = Number(inHeight.value);
     const isMale = inMale.checked;
     const isFemale = inFemale.checked;
     const value = isMale ? 22 : isFemale ? 21 : 0;
     const checkData =
         name !== "" &&
-        Number.isFinite(hight) &&
-        hight > 0 &&
-        hight < 3 &&
+        Number.isFinite(height) &&
+        height > 0 &&
+        height < 3 &&
         value !== 0;
 
     if (checkData) {
-        const result = value * (hight * 2);
+        const result = value * (height * 2);
         outResponse.textContent = `${name}: Your ideal weight is ${result} kg.`;
     }
 });
